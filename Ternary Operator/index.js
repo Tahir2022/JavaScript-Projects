626278f0bc1e06aeebde1b3c480e74f27a546874
Tahir2022 <abbasovtahir617@gmail.com>
2025-12-07 12:35:08 +0100

    Some exercises for ternary operator

--- Ternary Operator/index.js
+++ Ternary Operator/index.js
@@ -2,6 +2,18 @@
 //                    helps to assign a variable based on a condition
 //                    condition? codeIfTrue : codeIfFalse
 
-let age = 21;
-let message = age >= 18 ? "You are an adult" : "You are a minor";
-console.log(message);
+//let age = 21;
+//let message = age >= 18 ? "You are an adult" : "You are a minor";
+//console.log(message);
+
+//let time = 12;
+//let greeting = time < 12 ? "Good morning!" : "Good afternoon!";
+//console.log(greeting);
+
+//let isStudent = true;
+//let message = isStudent ? "You are student" : "You are not student";
+//console.log(message);
+
+let purchaseAmount = 50;
+let discount = purchaseAmount >= 100 ? 10 : 0;
+console.log(`Your total is $${purchaseAmount - purchaseAmount * (discount/100)}`)
