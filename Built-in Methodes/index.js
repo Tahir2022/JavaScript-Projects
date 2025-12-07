c6dfc1e81be22904f21f66cb8828127b353ac1cf
Tahir2022 <abbasovtahir617@gmail.com>
2025-12-07 13:40:44 +0100

    Repeated string methods

---
+++ Built-in Methodes/index.js
@@ -0,0 +1,23 @@
+//String methods = allow you to manipulate and work with text (strings)
+
+//let userName = "Tahir";
+//console.log(userName.charAt(3));
+
+//console.log(userName.indexOf("h"));
+
+//console.log(userName.trim());
+
+//console.log(userName.toUpperCase());
+
+//console.log(userName.repeat(10));
+
+//let result = userName.includes("r");
+//console.log(result);
+
+let phoneNummer = "123-456-7890"
+
+//phoneNummer = phoneNummer.replaceAll("-", "")
+//console.log(phoneNummer);
+
+phoneNummer = phoneNummer.padStart(20, "a");
+console.log(phoneNummer);
