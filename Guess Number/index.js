0416f84819fd5c4052242761e5d5af19bd0a98bf
Tahir2022 <abbasovtahir617@gmail.com>
2025-12-09 18:51:21 +0100

    Finished simple number guess app

--- Guess Number/index.js
+++ Guess Number/index.js
@@ -3,6 +3,33 @@
 const minNum = 1;
 const maxNum = 100;
 
-const answ = Math.floor(Math.random() * (maxNum - minNum +1));
+const answ = Math.floor(Math.random() * (maxNum - minNum +1)) + minNum;
 
-console.log(answ);
+let attemps = 0;
+let guess;
+let running = true;
+
+while(running){
+    guess = window.prompt(`Guess number between ${minNum} - ${maxNum}: `);
+    guess = Number(guess);
+
+    if(isNaN(guess)){
+        window.alert("Please enter a valid number");
+    }
+    else if(guess < minNum || guess > maxNum){
+        window.alert("Please enter a valid number")
+    }
+    else{
+        attemps++;
+        if(guess < answ){
+            window.alert("Too low! Try again");
+        }
+        else if(guess > answ){
+            window.alert("Too high!Try again");
+        }
+        else{
+            window.alert(`Correct! The answer was ${answ}. Your attemps: ${attemps}`);
+            running = false;
+        }
+    }
+}
