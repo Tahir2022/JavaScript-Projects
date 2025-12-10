356185e6d20bd331565de2570d19d3121a8700b6
Tahir2022 <abbasovtahir617@gmail.com>
2025-12-10 19:00:10 +0100

    Arrays

--- Built-in Methodes/main.js
+++ Built-in Methodes/main.js
@@ -119,3 +119,13 @@ function isValidEmail(email){
 console.log(isValidEmail("tahir@123"));
 */
 
+//2D array
+
+const matrix = [[1,2,3],
+                [4,5,6], 
+                [7,8,9]]
+
+for(let row of matrix){
+    const rowString = row.join(' ');
+    console.log(rowString);
+}
