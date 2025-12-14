cdbd2f049edc359372f946ff4d8ff91e7c0061c5
Tahir2022 <abbasovtahir617@gmail.com>
2025-12-14 11:37:54 +0100

    All done

--- Built-in Methodes/generator.js
+++ Built-in Methodes/generator.js
@@ -2,7 +2,33 @@
 
 function generatePassword(length, includeLowercase, includeNumbers, includeUppercase, includeSymbols){
 
-    return '';
+    const lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+    const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const numberChars = "0123456789";
+    const symbolChars = "!@#$%^&*()_+-=";
+
+    let allowedChars = "";
+    let password = "";
+
+    allowedChars += includeLowercase ? lowercaseChars : "";
+    allowedChars += includeUppercase ? uppercaseChars : "";
+    allowedChars += includeNumbers ? numberChars : "";
+    allowedChars += includeSymbols ? symbolChars : "";
+
+    if(length <= 0){
+        return `(password length must be at least 1)`;
+    }
+    if(allowedChars.length === 0){
+        return `(At least 1 set of character needs to be selected)`;
+    }
+
+    for(let i = 0; i < length; i++){
+        const randomIndex = Math.floor(Math.random() * allowedChars.length);
+        password += allowedChars[randomIndex];
+    }
+
+
+    return password;
 }
 
 
